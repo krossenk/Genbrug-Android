411fc2844833925dd17ae44897b4d96b8b49a900
Kristian Rossen Kristensen <krossenk@gmail.com>
2015-05-12 18:36:46 +0200

    Added Imagescaler

--- app/src/main/java/com/iha/genbrug/GiveActivity.java
+++ app/src/main/java/com/iha/genbrug/GiveActivity.java
@@ -7,10 +7,12 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -21,9 +23,11 @@ public class GiveActivity extends Activity implements View.OnClickListener {
     ImageButton btnCamPic;
     ImageButton btnBrowsePic;
     View relImageBackground;
+    TextView tvSize;
 
     ImageView imgView;
     private static int RESULT_LOAD_IMG = 1;
+    private static long MAX_IMAGE_SIZE = 150000;
     private String imgDecodableString;
 
     @Override
@@ -35,6 +39,7 @@ public class GiveActivity extends Activity implements View.OnClickListener {
         btnBrowsePic = (ImageButton) findViewById(R.id.btn_browse_pic);
         relImageBackground = findViewById(R.id.rel_image_background);
         imgView = (ImageView) findViewById(R.id.image_view);
+        tvSize = (TextView) findViewById(R.id.tv_image_size);
 
         btnCamPic.setOnClickListener(this);
         btnBrowsePic.setOnClickListener(this);
@@ -67,17 +72,32 @@ public class GiveActivity extends Activity implements View.OnClickListener {
                 cursor.close();
 
                 BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inSampleSize = 4;
-
                 File f = new File(imgDecodableString);
                 long imageByteSize = f.length();
+                options.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(imgDecodableString, options);
+                int beforeWidth = options.outWidth;
+                int beforeHeight = options.outHeight;
+                double divider = MAX_IMAGE_SIZE < beforeWidth*beforeHeight ? ((double)beforeWidth*beforeHeight / MAX_IMAGE_SIZE) : 1;
 
-               // if(imageByteSize > )
+
+                int optimalInSampleSize = (int) Math.round((Math.sqrt(divider)));
+                int i = 1;
+                while(i < optimalInSampleSize) {
+                    i *= 2;
+                }
+
+                options.inSampleSize = i;
+                options.inJustDecodeBounds = false;
 
                 // ImageView after decoding the String
                 Bitmap bitM = BitmapFactory.decodeFile(imgDecodableString, options);
-                //relImageBackground.setBackground(new BitmapDrawable(getResources(), bitM));
                 imgView.setImageBitmap(bitM);
+                tvSize.setText("ImageSizeBefore: " + beforeWidth + " " + beforeHeight + " " + imageByteSize + " ImageSizeAfter: " + options.outWidth + " " + options.outHeight + " " + options.outHeight * options.outWidth +
+                        " Multiplyer: " + options.inSampleSize);
+
+
+                //relImageBackground.setBackground(new BitmapDrawable(getResources(), bitM));
 
             } else {
                 Toast.makeText(this, "You haven't picked Image",
@@ -89,6 +109,14 @@ public class GiveActivity extends Activity implements View.OnClickListener {
         }
     }
 
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -96,12 +124,10 @@ public class GiveActivity extends Activity implements View.OnClickListener {
 
                 break;
             case R.id.btn_browse_pic:
-
                 Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, RESULT_LOAD_IMG);
-
-                //intent.setType("image/*");
-
+                if(isExternalStorageWritable()) {
+                    startActivityForResult(intent, RESULT_LOAD_IMG);
+                }
                 break;
         }
     }
