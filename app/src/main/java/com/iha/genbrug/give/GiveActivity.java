853d8a8de67bfd182fe73f7ade3e696c86c40489
Jonas Ulleberg Jussila <jonasjuss@gmail.com>
2015-05-21 17:46:21 +0200

    Added day time picker

--- app/src/main/java/com/iha/genbrug/give/GiveActivity.java
+++ app/src/main/java/com/iha/genbrug/give/GiveActivity.java
@@ -1,6 +1,8 @@
 package com.iha.genbrug.give;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -17,9 +19,10 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.NumberPicker;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.iha.genbrug.R;
@@ -28,6 +31,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -40,6 +44,9 @@ public class GiveActivity extends Activity implements View.OnClickListener {
         SHOW_IMAGE
     }
 
+    static final int DIALOG_ID = 0;
+    int hour, minute;
+
     InterceptScrollView interceptScrollview;
     ImageButton btnCamPic;
     ImageButton btnBrowsePic;
@@ -48,16 +55,15 @@ public class GiveActivity extends Activity implements View.OnClickListener {
     Button btnCropImage;
     Button btnGive;
     View relImageWrapper;
-    TextView tvSize;
     TouchImageView ivChosenImage;
     View layoutHorizontal;
     View layoutVertical;
 
     // FORM Views
     Spinner spinnerCategories;
-
-    Button mBtnUpdate;
-    TextView mTvUpdate;
+    Spinner spinnerPickupType;
+    Button btnPickStartTime;
+    Button btnPickEndTime;
 
     private static int RESULT_LOAD_IMG = 1;
     private static final int REQUEST_IMAGE_CAPTURE = 2;
@@ -72,9 +78,7 @@ public class GiveActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_give);
 
         interceptScrollview = (InterceptScrollView) findViewById(R.id.intercept_scrollview);
-        mBtnUpdate = (Button) findViewById(R.id.btn_update);
-        mTvUpdate = (TextView) findViewById(R.id.tv_update);
-        mBtnUpdate.setOnClickListener(this);
+
 
         layoutHorizontal = findViewById(R.id.layout_crop_grid_horizontal);
         layoutVertical = findViewById(R.id.layout_crop_grid_vertical);
@@ -87,11 +91,23 @@ public class GiveActivity extends Activity implements View.OnClickListener {
         btnGive = (Button) findViewById(R.id.btn_give);
         relImageWrapper = findViewById(R.id.rel_image_wrapper);
 
-        // FORM Views
+        // SPINNER CATEGORIES
         List<String> Categories =  Arrays.asList("Jonas", "Bikes", "Furniture", "Clothes", "Books");
         spinnerCategories = (Spinner) findViewById(R.id.spinner_category);
         ArrayAdapter<String> catAdapter = new CategoriesSpinnerAdapter(this, R.layout.categories_dropdown_item_layout, Categories);
         spinnerCategories.setAdapter(catAdapter);
+
+        // SPINNER PICKUP TYPE
+        List<String> PickupTypes = Arrays.asList("Home", "Street");
+        spinnerPickupType = (Spinner) findViewById(R.id.spinner_pickup_type);
+        ArrayAdapter<String> pickupAdapter = new PickupTypeSpinnerAdapter(this, R.layout.pickup_type_dropdown_item_layout, PickupTypes);
+        spinnerPickupType.setAdapter(pickupAdapter);
+
+        // BUTTON PICK START TIME
+        btnPickStartTime = (Button) findViewById(R.id.btn_pick_start_time);
+
+        // BUTTON PICK END TIME
+        btnPickEndTime = (Button) findViewById(R.id.btn_pick_end_time);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         //ArrayAdapter<CharSequence> categoriesAdapter = ArrayAdapter.createFromResource(this, R.array.categories_array, android.R.layout.simple_spinner_item);
@@ -102,8 +118,6 @@ public class GiveActivity extends Activity implements View.OnClickListener {
 
         // SOURCE: https://github.com/MikeOrtiz/TouchImageView
         ivChosenImage = (TouchImageView) findViewById(R.id.iv_chosen_image);
-
-        tvSize = (TextView) findViewById(R.id.tv_image_size);
 
         // Square the background image dynamically
         // SOURCE: http://stackoverflow.com/questions/9798392/imageview-have-height-match-width
@@ -124,12 +138,32 @@ public class GiveActivity extends Activity implements View.OnClickListener {
         btnRotatePic.setOnClickListener(this);
         btnCropImage.setOnClickListener(this);
         btnGive.setOnClickListener(this);
+        btnPickStartTime.setOnClickListener(this);
+        btnPickEndTime.setOnClickListener(this);
 
         setTouchImageViewState(TouchImageViewState.PICK_IMAGE);
 
         //Typeface font = Typeface.createFromAsset(getAssets(), "Sketchetik-Bold.otf");
         //btnCropImage.setTypeface(font);
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch(id){
+            case DIALOG_ID:
+                return new TimePickerDialog(this, mTimeSetListener, hour, minute, true );
+        }
+        return null;
+    }
+
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int i, int i2) {
+            hour = i;
+            minute = i2;
+            Toast.makeText(getBaseContext(), "Set time is: " + hour + ":" + minute, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     private int getImageRotation(String path){
         ExifInterface exif;
@@ -234,9 +268,6 @@ public class GiveActivity extends Activity implements View.OnClickListener {
 
                 setmDisplayedBitmap(rotatedBitmap);
 
-
-                tvSize.setText("OptimalInSampleSize: " + optimalInSampleSize + " Orientation: " + rotation + " ImageSizeBefore: " + beforeWidth + " " + beforeHeight + " " + imageByteSize + " ImageSizeAfter: " + rotatedBitmap.getWidth() + " " + rotatedBitmap.getHeight() + " " + rotatedBitmap.getWidth() * rotatedBitmap.getHeight() +
-                        " Multiplyer: ");
                 setTouchImageViewState(TouchImageViewState.CROP_IMAGE);
             }
 
@@ -296,9 +327,67 @@ public class GiveActivity extends Activity implements View.OnClickListener {
                 setmDisplayedBitmap(rotated);
                 ivChosenImage.setScrollPosition((1-tempY), tempX);
                 break;
+            case R.id.btn_pick_start_time:
+                showDialog(DIALOG_ID);
+                break;
+            case R.id.btn_pick_end_time:
+                final Dialog petDialog = new Dialog(this);
+                petDialog.setTitle("Select latest pickup time");
+                petDialog.setContentView(R.layout.custom_dialog_date_time);
+                petDialog.show();
 
-            case R.id.btn_update:
-                mTvUpdate.setText(ivChosenImage.getScrollPosition().toString());
+                // ---------------------------------------------------------------------------------
+                // DAY DATE PICKER (using a NumberPicker)
+                // ---------------------------------------------------------------------------------
+                final NumberPicker npDayDate = (NumberPicker) petDialog.findViewById(R.id.np_day_date);
+
+                final String[] dayDateValues = new String[14];
+                final Date date = new Date(); // Get today's date
+
+                for(int i = 0; i < dayDateValues.length; i++){
+                    Calendar c = Calendar.getInstance();
+                    Date futureDate = new Date();
+                    c.setTime(date);            // Set today's date to calender
+                    c.add(Calendar.DATE, i);    // Increment date by i
+                    futureDate = c.getTime();
+
+                    SimpleDateFormat sdf = new SimpleDateFormat("EE, MMM dd"); // Format as "Tue, Jul 21"
+                    dayDateValues[i] = sdf.format(futureDate);  // Add day date to values
+                }
+                npDayDate.setMaxValue(dayDateValues.length - 1);
+                npDayDate.setMinValue(0);
+                npDayDate.setDisplayedValues(dayDateValues);
+                npDayDate.setWrapSelectorWheel(false);
+
+                // ---------------------------------------------------------------------------------
+                // TIME PICKER
+                // ---------------------------------------------------------------------------------
+                TimePicker tpTime = (TimePicker) petDialog.findViewById(R.id.tp_time);
+                tpTime.setIs24HourView(true); // Enable 24 hour mode
+
+
+                // ---------------------------------------------------------------------------------
+                // DIALOG BUTTONS
+                // ---------------------------------------------------------------------------------
+                Button btnSubmitDialog = (Button) petDialog.findViewById(R.id.btn_submit_dialog);
+                Button btnCancelDialog = (Button) petDialog.findViewById(R.id.btn_cancel_dialog);
+
+                btnSubmitDialog.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getApplicationContext(), ( dayDateValues[npDayDate.getValue()]), Toast.LENGTH_SHORT).show();
+                        petDialog.cancel();
+                    }
+                });
+
+                btnCancelDialog.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getApplicationContext(),"cancelled", Toast.LENGTH_SHORT).show();
+                        petDialog.cancel();
+                    }
+                });
+
                 break;
         }
     }
