f5e2f5c16af933beee853927c51e27addd4404fb
vindenergi <py@vindenergi.dk>
2015-05-25 12:01:52 +0200

    small changes

--- app/src/main/java/com/iha/genbrug/DetailActivity.java
+++ app/src/main/java/com/iha/genbrug/DetailActivity.java
@@ -5,12 +5,10 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,19 +16,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
-import com.google.gson.Gson;
-
+import webservice.Subscription;
 import webservice.User;
+import webservice.getUserSubscriptionsResponse;
 
 
 public class DetailActivity extends Activity {
 
-    Intent intent;
-    Button infoBtn;
     Button logOutBtn;
-    SharedPreferences prefs;
-    private int itemId;
+    private long itemId;
     private ServerService serverServiceSubscribe;
+    private getUserSubscriptionsResponse subscriptionsResponse;
     private GlobalSettings  globalSettings =GlobalSettings.getInstance();
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
@@ -59,7 +55,7 @@ public class DetailActivity extends Activity {
         String imageId =(bundle.getString("imageId"));
         String header = (bundle.getString("headline"));
         String desc = (bundle.getString("desc"));
-        itemId = (bundle.getInt("itemId"));
+        itemId = (bundle.getLong("itemId"));
         Bitmap bm = BitmapFactory.decodeResource(getResources(), Integer.parseInt(imageId));
         ImageView imageView = (ImageView) findViewById(R.id.item_photo);
         imageView.setImageBitmap(bm);
@@ -81,8 +77,6 @@ public class DetailActivity extends Activity {
     // temporary logout function
     public void logOut(View v)
     {
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-
 
         //Reset all information from user
         globalSettings.sharedPreferences.edit().putString("userObj", "").commit();
@@ -100,11 +94,24 @@ public class DetailActivity extends Activity {
         User user = globalSettings.getUserFromPref();
         long  userId = user.id;
 
+        //getUserSubscriptionsResponse list;
+        //list = serverServiceSubscribe.getUserSubscriptions();
 
         if (userId != 0)
         {
-            serverServiceSubscribe.createSubscription(userId,itemId);
-            Toast.makeText(this,"The item with itemId: " + itemId + " is subscribed",Toast.LENGTH_SHORT).show();
+
+            if(subscriptionsResponse != null) {
+                for (Subscription sub : subscriptionsResponse) {
+                    if (itemId != sub.publicationId.id)
+                        serverServiceSubscribe.createSubscription(userId, itemId);
+                    Toast.makeText(this, "The item with itemId: " + itemId + " is subscribed", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else{
+
+                serverServiceSubscribe.createSubscription(userId, itemId);
+                Toast.makeText(this, "The item with itemId: " + itemId + " is subscribed", Toast.LENGTH_SHORT).show();
+            }
         }
 
              else
