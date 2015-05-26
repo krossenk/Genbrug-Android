6a62f8fa84db6e7fe84e2a747f503e85e8c82252
Kristian Rossen Kristensen <krossenk@gmail.com>
2015-05-26 20:29:00 +0200

    Added Profile Tracker to fix Facebook login failure

--- app/src/main/java/com/iha/genbrug/FBFragment.java
+++ app/src/main/java/com/iha/genbrug/FBFragment.java
@@ -2,10 +2,8 @@ package com.iha.genbrug;
 
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -18,12 +16,9 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
 
 import webservice.User;
 
@@ -36,42 +31,43 @@ public class FBFragment extends Fragment {
 
     private CallbackManager mcallbackManager;
     private View view;
-    LoginButton loginButton;
+    private LoginButton loginButton;
     private GlobalSettings globalSettings;
+    private ProfileTracker mProfileTracker;
 
-    // Callback method for login with facebook
+    // Callback method for login with facebook.
     private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
         @Override
         public void onSuccess(LoginResult loginResult) {
+            // Creates profile tracker to get the newest profile information.
+            mProfileTracker = new ProfileTracker() {
+                @Override
+                protected void onCurrentProfileChanged(Profile profile1, Profile profile) {
+                    if(profile != null) {
+                        Uri profilePictureUri = profile.getProfilePictureUri(400, 300);
+                        loginButton.setVisibility(View.INVISIBLE);
 
-            Intent intent = new Intent(getActivity(),MainActivity.class);
+                        globalSettings = GlobalSettings.getInstance();
+                        User user = new User();
 
-            Profile profile = Profile.getCurrentProfile();
-            if (profile != null) {
+                        user.id = Long.valueOf(profile.getId());
+                        user.firstname = profile.getFirstName();
+                        user.lastname = profile.getLastName();
+                        user.profileimageURL = profilePictureUri.toString();
 
-                Uri profilePictureUri = profile.getProfilePictureUri(400, 300);
-                loginButton.setVisibility(View.INVISIBLE);
+                        globalSettings.saveUserToPref(user);
+                        globalSettings.sharedPreferences.edit().putBoolean("Islogin", true).commit();
 
-                 globalSettings = GlobalSettings.getInstance();
-                 User user = new User();
+                        Toast.makeText(getActivity(), "Welcome " + profile.getName(),
+                                Toast.LENGTH_SHORT).show();
 
-                user.id = Long.valueOf(profile.getId());
-                user.firstname = profile.getFirstName();
-                user.lastname = profile.getLastName();
-                user.profileimageURL = profilePictureUri.toString();
-
-
-
-                globalSettings.saveUserToPref(user);
-                globalSettings.sharedPreferences.edit().putBoolean("Islogin", true).commit();
-
-                Toast.makeText(getActivity(), "Welcome " + profile.getId(),
-                        Toast.LENGTH_SHORT).show();
-
-                startActivity(intent);
-                getActivity().finish();
-
-            }
+                        Intent intent = new Intent(getActivity(),MainActivity.class);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }
+                    mProfileTracker.stopTracking();
+                }
+            };
         }
 
 
